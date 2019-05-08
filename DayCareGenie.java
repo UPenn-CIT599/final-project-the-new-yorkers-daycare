210d3f7755c01a44b49986f6a726e26c9dc10a07
sswang39 <stevewang@stanfordalumni.org>
2019-05-08 16:15:42 -0400

    Added method to get most recent inspection record

--- DayCareGenie.java
+++ DayCareGenie.java
@@ -1,5 +1,7 @@
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.*;
 
 public class DayCareGenie {
 	//variable list
@@ -10,15 +12,18 @@ public class DayCareGenie {
 		list = data;
 	}
 	
-	//Method to retreive 
-	ArrayList <DayCareProviderModel> getDayCaresByZipcode (String zipcode) {
+	//Method to retrieve inspections of all day care centers in a zipcode
+	public ArrayList <DayCareProviderModel> getDayCaresByZipcode (String zipcode) {
 		//methods to answer question using variable list
 		ArrayList<DayCareProviderModel> result = new ArrayList<DayCareProviderModel>();
 		
 		for(DayCareProviderModel dc : list) {
-			if (dc.getZipcode().equals(zipcode) == true) {
-				result.add(dc);
+			if (dc.getZipcode() != null) {
+				if (dc.getZipcode().equals(zipcode)) {
+					result.add(dc);
+				}
 			}
+			
 		}
 		
 		return result;
@@ -26,12 +31,12 @@ public class DayCareGenie {
 	
 	/**
 	 * gets all day care providers with the specified zipcode whose last inspection date
-	 * is greater than or equal to the specified inpsection date.
+	 * is greater than or equal to the specified inspection date.
 	 * @param zipcode - a String representing the 6 digit zipcode to look for.
 	 * @param inspectionDate - a date representing the desired inspection date
 	 * @return a list of Day Care Providers matching the passed parameters.
 	 */
-	ArrayList <DayCareProviderModel> getDayCaresByZipcode (String zipcode, Date inspectionDate) {
+	public ArrayList <DayCareProviderModel> getDayCaresByZipcode (String zipcode, Date inspectionDate) {
 		//methods to answer question using variable list
 		ArrayList<DayCareProviderModel> result = new ArrayList<DayCareProviderModel>();
 		
@@ -44,5 +49,43 @@ public class DayCareGenie {
 		
 		return result;
 	}
-}
+	
+	public HashMap<String, DayCareProviderModel> getMostRecentInspections(ArrayList<DayCareProviderModel> inspectionsByZipCode) {
+		/**
+		 * This method takes in an arraylist of all the inspections for a zip code.
+		 * It iterates through those inspections and extracts the most recent inspection for
+		 * each day care center.
+		 * Each center and its most recent inspection are stored as key-values in a 
+		 * hashmap, which is returned
+		 */
+		
+		// hashmap to store the most recent inspections for the centers in the zip code
+		HashMap<String, DayCareProviderModel> mostRecentInspections = new HashMap<String, DayCareProviderModel>();
 
+		
+		// iterate through inspections for a zipcode
+		for (DayCareProviderModel inspection : inspectionsByZipCode) {
+			// determine if a center is in the hashmap
+			// if so, determine if this inspection record is more recent than 
+			// the one stored in the hashmap; if so, replace the value in the hashmap
+			// with this inspection record
+			// else add the center and inspection record to the hashmap
+			if (mostRecentInspections.containsKey(inspection.getCenterName())) {
+				if (inspection.getInspectiondate() != null) {
+					Date comparisonDate = mostRecentInspections.get(inspection.getCenterName()).getInspectiondate();
+					if (inspection.getInspectiondate().after(comparisonDate)) {
+					mostRecentInspections.put(inspection.getCenterName(), inspection);
+					}
+				}
+				
+			} else {
+				if (inspection.getInspectiondate()!= null) {
+					mostRecentInspections.put(inspection.getCenterName(), inspection);
+				}
+				
+			}
+		}
+		
+		return mostRecentInspections;
+	}
+}
